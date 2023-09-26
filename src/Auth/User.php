eabde8d23b6e27a212fa4db23f04171a5b82187c
Sasaya <storyn26383@gmail.com>
2023-09-26 10:59:40 +0800

    feat: add gate traits

--- src/Auth/User.php
+++ src/Auth/User.php
@@ -4,11 +4,14 @@ declare(strict_types=1);
 
 namespace SwooleTW\Hyperf\Foundation\Auth;
 
+use SwooleTW\Hyperf\Auth\Access\Authorizable;
 use SwooleTW\Hyperf\Auth\Authenticatable;
 use SwooleTW\Hyperf\Auth\Contracts\Authenticatable as AuthenticatableContract;
+use SwooleTW\Hyperf\Auth\Contracts\Authorizable as AuthorizableContract;
 use SwooleTW\Hyperf\Foundation\Model\Model;
 
-class User extends Model implements AuthenticatableContract
+class User extends Model implements AuthenticatableContract, AuthorizableContract
 {
     use Authenticatable;
+    use Authorizable;
 }
