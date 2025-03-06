6d13b2998637bff4a180f8dae771532448bec618
albertcht <9117929+albertcht@users.noreply.github.com>
2025-03-06 14:06:14 +0000

    Update version to v0.1.9

--- src/Application.php
+++ src/Application.php
@@ -31,7 +31,7 @@ class Application extends Container implements ApplicationContract
      *
      * @var string
      */
-    public const VERSION = '0.1.8';
+    public const VERSION = '0.1.9';
 
     /**
      * The base path for the Laravel Hyperf installation.
