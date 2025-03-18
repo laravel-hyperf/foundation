2337c9e99ecc0ad5130f81e50d499eeda9176f0f
storyn26383 <6954098+storyn26383@users.noreply.github.com>
2025-03-18 15:00:47 +0000

    Update version to v0.1.20

--- src/Application.php
+++ src/Application.php
@@ -31,7 +31,7 @@ class Application extends Container implements ApplicationContract
      *
      * @var string
      */
-    public const VERSION = '0.1.19';
+    public const VERSION = '0.1.20';
 
     /**
      * The base path for the Laravel Hyperf installation.
